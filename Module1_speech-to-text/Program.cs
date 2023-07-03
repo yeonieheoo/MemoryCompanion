914282b8939afd3405857c04969911a04e24df56
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2023-07-03 22:43:34 +0800

    added module 1 and module 3

---
+++ Module1_speech-to-text/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+// This example uses the RecognizeOnceAsync operation to transcribe 
+// utterances of up to 30 seconds, or until silence is detected. 
+// For information about continuous recognition for longer audio, 
+// including multi-lingual conversations, see How to recognize speech.
+
+class Program 
+{
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    // static string speechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
+    // static string speechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");
+    static string speechKey = "c324d625aeed4f179d9c321d92343ded";
+    static string speechRegion = "uaenorth";
+
+
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+    {
+        switch (speechRecognitionResult.Reason)
+        {
+            case ResultReason.RecognizedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+        }
+    }
+
+    async static Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);        
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(speechRecognitionResult);
+    }
+}
